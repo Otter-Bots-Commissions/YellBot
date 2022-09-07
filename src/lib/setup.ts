9f620b563caaf9e9a3a95ba913fa819d506c64c7
Devramsean0 <81807361+devramsean0@users.noreply.github.com>
2022-09-07 19:51:00 +0100

    Hell if i know

--- src/lib/setup.ts
+++ src/lib/setup.ts
@@ -5,9 +5,17 @@ import 'reflect-metadata';
 import '@sapphire/plugin-logger/register';
 import * as colorette from 'colorette';
 import { inspect } from 'util';
-
-// Set default inspection depth
+import "dotenv/config";
+import { container } from '@sapphire/framework';
+import { QuickDB } from "quick.db";
+container.db = new QuickDB();
 inspect.defaultOptions.depth = 1;
 
 // Enable colorette
 colorette.createColors({ useColor: true });
+
+declare module "@sapphire/pieces" {
+    interface Container {
+        db: QuickDB;
+    }
+}
--- src/preconditions/OwnerOnly.ts
+++ src/preconditions/OwnerOnly.ts
@@ -1,8 +1,6 @@
 import { AllFlowsPrecondition } from '@sapphire/framework';
 import type { CommandInteraction, ContextMenuInteraction, Message, Snowflake } from 'discord.js';
-import { envParseArray } from '../lib/env-parser';
-
-const OWNERS = envParseArray('OWNERS');
+const OWNERS = process.env.Owners?.split("")
 
 export class UserPrecondition extends AllFlowsPrecondition {
 	#message = 'This command can only be used by the owner.';
@@ -20,7 +18,7 @@ export class UserPrecondition extends AllFlowsPrecondition {
 	}
 
 	private doOwnerCheck(userId: Snowflake) {
-		return OWNERS.includes(userId) ? this.ok() : this.error({ message: this.#message });
+		return OWNERS?.includes(userId) ? this.ok() : this.error({ message: this.#message });
 	}
 }
 
